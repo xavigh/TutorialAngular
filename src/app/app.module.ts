1f907bcce0248f1151190be3c69acec2db1440f2
xaviGH <fgargallo@gmail.com>
2019-07-16 18:33:36 +0200

    first project6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectornumericoComponent
  
     
   ],
---
+++ src/app/selectornumerico/selectornumerico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectornumerico',
+  templateUrl: './selectornumerico.component.html',
+  styleUrls: ['./selectornumerico.component.css']
+})
+export class SelectornumericoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
