bf668cdc6a607c2f12b56f727458d90d1a87b58a
xaviGH <fgargallo@gmail.com>
2019-07-16 18:31:41 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CronometroComponent } from './cronometro/cronometro.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CronometroComponent
+    AppComponent
+ 
     
   ],
   imports: [
